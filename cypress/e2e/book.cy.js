58908d380324ac9e638b65d7e817455b0fb6827f
Carol Protásio <protasio.carol@gmail.com>
2024-10-08 17:53:41 -0300

    changing server

--- cypress/e2e/book.cy.js
+++ cypress/e2e/book.cy.js
@@ -11,7 +11,7 @@ describe('/book CRUD', () => {
     })
   })
 
-  it('CT-001 should register a new book', function () {
+  it('CT-001 POST - should register a new book', function () {
     const book = this.books.create
 
     cy.postNewBook(book)
@@ -23,7 +23,7 @@ describe('/book CRUD', () => {
       })
 
   })
-  it('CT-002 should not register book with a duplicate title', function() {
+  it('CT-002 POST - should not register book with a duplicate title', function () {
     const book = this.books.duplicate
 
     cy.postNewBook(book)
@@ -33,29 +33,63 @@ describe('/book CRUD', () => {
         expect(response.body.erro).eql('O livro já foi cadastrado!')
       })
   })
-  it('CT-003 should get all registed books', function() {
+  it('CT-003 GET - should get all registed books', function () {
     const book1 = this.books.manyBooks[0]
     const book2 = this.books.manyBooks[1]
     const book3 = this.books.manyBooks[2]
+    const expectedBooks = [book1, book2, book3]
 
-    cy.postNewBook(book1) 
+    cy.postNewBook(book1)
     cy.postNewBook(book2)
-    cy.postNewBook(book3)  
+    cy.postNewBook(book3)
 
-    const expectedBooks = [book1, book2, book3 ]
-    
-    cy.getBooks() 
-      .then(response => {    
+    cy.getBooks()
+      .then(response => {
+        expect(response.status).eql(200)
 
         expectedBooks.forEach((expectedBook) => {
-        const foundBook = response.body.find(book => book.title === expectedBook.title);
-        expect(foundBook).to.exist; 
-        expect(foundBook.author).to.equal(expectedBook.author);
-        expect(foundBook.publisher).to.equal(expectedBook.publisher);
-        expect(foundBook.year).to.equal(expectedBook.year);
-        expect(foundBook.pages).to.equal(expectedBook.pages);
-      });
-    })
+          const foundBook = response.body.find(book => book.title === expectedBook.title);
+          expect(foundBook).to.exist;
+          expect(foundBook.author).to.equal(expectedBook.author);
+          expect(foundBook.publisher).to.equal(expectedBook.publisher);
+          expect(foundBook.year).to.equal(expectedBook.year);
+          expect(foundBook.pages).to.equal(expectedBook.pages);
+        });
+      })
+  })
+  it('CT-004 GET - should get a book by ID', function () {
+    const book = this.books.create
+
+    cy.postNewBook(book)
+      .then(response => {
+        const bookId = response.body._id
+        cy.log('ID do livro criado:', bookId);
+
+        cy.getBookById(bookId)
+          .then(bookResp => {
+            expect(bookResp.status).eql(200)
+            expect(bookResp.body._id).to.eq(bookId)
+
+          })
+      })
+
+  })
+  it('CT-004 DELETE - should delete a book by ID', function () {
+    const book = this.books.delete
+    
+
+    cy.postNewBook(book)    
+      .then(response => {
+        const bookId = response.body._id
+        cy.log('ID do livro criado:', bookId);
+
+        cy.deleteBookById(bookId)
+          .then(response => {
+            expect(response.status).to.eql(200)
+            expect(response.body.message).to.have.text('Livro removido com sucesso.')
+
+          })
+      })
   })
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,10 +8,24 @@ Cypress.Commands.add('postNewBook', (book) => {
       }).then(response => {return response })
  })
 
+Cypress.Commands.add('getBookById', (id) => { 
+    cy.api({
+        url: '/api/books/${id}',
+        method: 'GET',        
+        failOnStatusCode: false,
+      }).then(response => {return response })
+ })
 Cypress.Commands.add('getBooks', () => { 
     cy.api({
         url: '/api/books',
         method: 'GET',        
+        failOnStatusCode: false,
+      }).then(response => {return response })
+ })
+ Cypress.Commands.add('deleteBookById', (id) => { 
+    cy.api({
+        url: '/api/books/${id}',
+        method: 'DELETE',        
         failOnStatusCode: false,
       }).then(response => {return response })
  })
